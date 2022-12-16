fb6615b9d76e386402127a67d59390eb166a3a2e
Behzad Rabiei <behzad.rabiei.77@gmail.com>
2022-12-16 22:26:16 +0400

    add index

---
+++ index.ts
@@ -0,0 +1,5 @@
+import { User, Token, Guild } from './src/models';
+import { IDiscordGuild, IDiscordOathBotCallback, IDiscordUser } from './src//interfaces/Discord.interface';
+import { IUser, IUserModel } from './src/interfaces/User.interface';
+import { IToken, ITokenModel } from './src/interfaces/Token.interface';
+import { IGuild, IGuildModel } from './src/interfaces/Guild.interface';
