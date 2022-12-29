455a7813331a981237134a473b38fe257299ec83
Behzad Rabiei <behzad.rabiei.77@gmail.com>
2022-12-29 16:02:25 +0400

    convert models to schemas

---
+++ src/schemas/RawInfo.schema.ts
@@ -0,0 +1,36 @@
+import { Schema } from 'mongoose';
+import { toJSON } from './plugins';
+import { IRawInfo } from '../interfaces/RawInfo.interface';
+
+const rawInfoSchema = new Schema<IRawInfo>({
+    type: {
+        type: String
+    },
+    author: {
+        type: String
+    },
+    content: {
+        type: String
+    },
+    user_Mentions: [{
+        type: String
+    }],
+    roles_Mentions: [{
+        type: String
+    }],
+    reactions: [{
+        type: String
+    }],
+    replied_User: {
+        type: String
+    },
+    reference_Message: {
+        type: Number
+    }
+
+});
+
+// Plugins
+rawInfoSchema.plugin(toJSON);
+
+export default rawInfoSchema;
---
+++ src/schemas/Token.schema.ts
@@ -0,0 +1,37 @@
+import { Schema } from 'mongoose';
+import { toJSON } from './plugins';
+import { tokenTypes } from '../config/tokens';
+import { IToken } from '../interfaces/Token.interface';
+
+const tokenSchema = new Schema<IToken>(
+    {
+        token: {
+            type: String,
+            required: true,
+            index: true,
+        },
+        user: {
+            type: String,
+            ref: 'User',
+            required: true,
+        },
+        type: {
+            type: String,
+            enum: Object.values(tokenTypes),
+            required: true,
+        },
+        expires: {
+            type: Date,
+            required: true,
+        },
+        blacklisted: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    { timestamps: true });
+
+// Plugins
+tokenSchema.plugin(toJSON);
+
+export default tokenSchema;
