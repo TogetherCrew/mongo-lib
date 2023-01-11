efd2711f6c02379cdeca148efbce852fe5a25389
Raymond <magicpalmtree.space@gmail.com>
2023-01-11 12:38:15 -0500

    Feat: fetch messages by settings with selected channels and period

--- src/service/rawInfo.service.ts
+++ src/service/rawInfo.service.ts
@@ -1,15 +1,33 @@
 import { Connection } from 'mongoose';
-import { IRawInfo } from '../interfaces/RawInfo.interface';
+import { IRawInfo, IGuild } from '../interfaces';
+import moment = require("moment");
 
 /**
  * Create rawInfo
  * @param {IRawInfo} data
  * @returns {Promise<IRawInfo>}
- */
+*/
 async function createRawInfo(connection: Connection, data: IRawInfo) {
     return connection.models.RawInfo.create(data);
 }
 
+const fetchRawinfo = async (connection: Connection, {selectedChannels, period}: IGuild) => {
+    const model = connection.models.RawInfo;
+    const channelList = selectedChannels?.map((item) => item.channelId);
+    const today = moment().startOf('day');
+    const data = await model.find({
+        channelId: { 
+            $in:channelList,
+        },
+        created_at: {
+            $gte: moment(period).toDate(),
+            $lte: moment(today).endOf('day').toDate()
+        }
+    });
+    return data;
+}
+
 export default {
-    createRawInfo
+    createRawInfo,
+    fetchRawinfo
 }
