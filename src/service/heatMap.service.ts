e04d9b59e16319aec9d9c300dcfa4df749659cb3
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2023-01-27 13:15:56 +0400

    function to insert many documents to heatmap collection

--- src/service/heatMap.service.ts
+++ src/service/heatMap.service.ts
@@ -10,6 +10,16 @@ async function createHeatMap(connection: Connection, data: IHeatMap) {
     return connection.models.HeatMap.create(data);
 }
 
+/**
+ * Create heatmaps
+ * @param {IHeatMap} data
+ * @returns {Promise<IHeatMap>}
+ */
+async function createHeatMaps(connection: Connection, data: [IHeatMap]) {
+    return connection.models.HeatMap.insertMany(data);
+}
+
 export default {
-    createHeatMap
+    createHeatMap,
+    createHeatMaps
 }
