738b1d08c1ead5ef5544309336d283fd89976eed
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2023-01-31 13:11:47 +0400

    fix errors

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import { User, Token, Guild, HeatMap, RawInfo } from './models';
 import { IDiscordGuild, IDiscordOathBotCallback, IDiscordUser } from './interfaces/Discord.interface';
 import { IUser, IUserModel } from './interfaces/User.interface';
 import { IToken, ITokenModel } from './interfaces/Token.interface';
-import { IGuild, IGuildModel } from './interfaces/Guild.interface';
+import { IGuild, GuildModel } from './interfaces/Guild.interface';
 import { databaseService, heatmapService, rawInfoService, guildService } from './service';
 
 
 
 
 export {
-    User, Token, Guild, HeatMap, RawInfo, IDiscordGuild, IDiscordOathBotCallback, IDiscordUser, IUser, IUserModel, IToken, ITokenModel, IGuild, IGuildModel,
+    User, Token, Guild, HeatMap, RawInfo, IDiscordGuild, IDiscordOathBotCallback, IDiscordUser, IUser, IUserModel, IToken, ITokenModel, IGuild, GuildModel,
     databaseService, heatmapService, rawInfoService, guildService
 }
