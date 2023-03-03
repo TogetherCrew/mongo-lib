b3ffd28f7a17d7ba97f025672f5ab85f263e089f
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2023-03-03 18:51:04 +0400

    [FIXBUG]

--- __tests__/unit/models/heatMap.model.test.ts
+++ __tests__/unit/models/heatMap.model.test.ts
@@ -1,13 +1,12 @@
 import { HeatMap } from '../../../src/models';
 import { IHeatMap } from '../../../src/interfaces/HeatMap.interface'
-import moment from "moment";
 
 describe('HeatMap model', () => {
     describe('heatMap validation', () => {
         let heatMap: IHeatMap;
         beforeEach(() => {
             heatMap = {
-                date: moment("2022-02-01 08:30:26.127Z").toDate(),
+                date: "2022-02-01",
                 channel: "Human vibez",
                 thr_messages: [0, 1, 1, 1, 2, 0, 0, 1, 1, 0, 1, 0, 2, 0, 4, 3, 1, 2, 0, 1, 0, 1, 0, 2],
                 lone_messages: [0, 1, 1, 1, 2, 0, 0, 1, 1, 0, 1, 0, 2, 0, 4, 3, 1, 2, 0, 1, 0, 1, 0, 2],
