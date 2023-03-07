90e7113cae69688b6698d70222312f21139418cc
Sortoite <zachatatem@gmail.com>
2023-03-07 09:40:09 -0600

    Feat: update database service with channel

--- src/service/channels.service.ts
+++ src/service/channels.service.ts
@@ -1,14 +1,17 @@
+import { Connection } from 'mongoose';
 import { IChannels } from '../interfaces/Channels.interface';
 import { Snowflake } from 'discord.js';
-import { Channels} from '../models'
 
 /**
  * Create channel
  * @param {IChannels} data
  * @returns {Promise<IChannels>}
  */
-const createChannel = async (data: IChannels) => {
-    return Channels.create(data);
+const createChannel = async (connection: Connection, data: IChannels) => {
+    const Channels = connection.models.Channels;
+    const now = new Date();
+    const dataWithTimestamp = { ...data, last_update: now };
+    return await Channels.create(dataWithTimestamp);
 }
 
 
@@ -18,8 +21,9 @@ const createChannel = async (data: IChannels) => {
  * @param {String} channel
  * @returns {Promise<IChannels>}
  */
-const updateChannel = async (channelId: Snowflake, channel: string) => {
+const updateChannel = async (connection: Connection, channelId: Snowflake, channel: string) => {
     try {
+        const Channels = connection.models.Channels;
         await Channels.updateOne(
         { channelId: channelId },
         { 
@@ -27,7 +31,6 @@ const updateChannel = async (channelId: Snowflake, channel: string) => {
             $currentDate: { last_update: true }
         }
      )
-
     } catch(e) {
         console.log(e);
     }
