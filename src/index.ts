36758b1a60e97f21ce873c9e84a60ca4acfc9dd1
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2023-03-13 16:47:35 +0400

    [FEATURE]: export memberActivity model and interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import { User, Token, Guild, HeatMap, RawInfo } from './models';
+import { User, Token, Guild, HeatMap, RawInfo, MemberActivity } from './models';
 import { IDiscordGuild, IDiscordOathBotCallback, IDiscordUser } from './interfaces/Discord.interface';
 import { IUser, UserModel } from './interfaces/User.interface';
 import { IToken, TokenModel } from './interfaces/Token.interface';
 import { IGuild, GuildModel } from './interfaces/Guild.interface';
-import { databaseService, heatmapService, rawInfoService, guildService, channelsService} from './service';
+import { IMemberActivity, MemberActivityModel } from './interfaces/MemberActivity.interface';
+
+import { databaseService, heatmapService, rawInfoService, guildService, channelsService } from './service';
 
 export {
-    User, Token, Guild, HeatMap, RawInfo, IDiscordGuild, IDiscordOathBotCallback, IDiscordUser, IUser, UserModel, IToken, TokenModel, IGuild, GuildModel,
+    User, Token, Guild, HeatMap, RawInfo, MemberActivity, IDiscordGuild, IDiscordOathBotCallback, IDiscordUser, IUser, IMemberActivity, MemberActivityModel, UserModel, IToken, TokenModel, IGuild, GuildModel,
     databaseService, heatmapService, rawInfoService, guildService, channelsService
 }
