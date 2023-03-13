cc94e99a3cc577b6f22f7a6c70578c0779415ce0
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2023-03-13 16:45:49 +0400

    [FEATURE]: add memberActivity model to each guild database

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,13 +1,15 @@
-import {IGuild} from './Guild.interface';
-import {IRawInfo} from './RawInfo.interface';
-import {IHeatMap} from './HeatMap.interface';
-import {IToken} from './Token.interface';
-import {IChannels} from './Channels.interface';
+import { IGuild } from './Guild.interface';
+import { IRawInfo } from './RawInfo.interface';
+import { IHeatMap } from './HeatMap.interface';
+import { IToken } from './Token.interface';
+import { IChannels } from './Channels.interface';
+import { IMemberActivity } from './MemberActivity.interface';
 
 export {
     IGuild,
     IRawInfo,
     IToken,
     IHeatMap,
-    IChannels
+    IChannels,
+    IMemberActivity
 }
