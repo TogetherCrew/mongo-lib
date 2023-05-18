6d50e7bd412bf6c538c03a592c7e314446762daa
Behzad Rabiei <behzad.rabiei.77@gmail.com>
2023-05-18 13:48:36 +0400

    [TEST]: update tests

--- __tests__/unit/models/guildMember.model.test.ts
+++ __tests__/unit/models/guildMember.model.test.ts
@@ -1,5 +1,6 @@
 import { GuildMember } from '../../../src/models';
 import { IGuildMember } from '../../../src/interfaces/GuildMember.interface';
+import moment from "moment";
 
 describe('Account model', () => {
   describe('account validation', () => {
@@ -7,10 +8,12 @@ describe('Account model', () => {
     beforeEach(() => {
       guildMember = {
         discordId: '123',
-        nick: 'Behzad',
+        username: 'Behzad#11',
         roles: ['admin'],
-        joined_at: "2000/10/15",
-        avatar: "100"
+        joinedAt: moment("2022-02-01 08:30:26.127Z").toDate(),
+        avatar: "100",
+        isBot: true,
+        discriminator: 'str'
       };
     });
     test('should correctly validate a valid guildMember data', async () => {
