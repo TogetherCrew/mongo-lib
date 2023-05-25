47f3fa432ad2680ce801b45ee5464d44550b4bc8
Cyrille <1618640+cyri113@users.noreply.github.com>
2023-05-25 16:21:53 +0200

    Update jest.config

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-    preset: 'ts-jest',
-    transform: {
-        '^.+\\.(ts|tsx)?$': 'ts-jest',
-        "^.+\\.(js|jsx)$": "babel-jest",
-    },
-    modulePathIgnorePatterns: ["utils", "fixtures"]
-};
