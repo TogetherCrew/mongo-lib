d3324a637d612310c02974a7c037317ea9920d24
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2023-09-14 16:13:38 +0400

    [FEATURE]: add twitter token types

--- src/config/tokens.ts
+++ src/config/tokens.ts
@@ -4,5 +4,5 @@ export const tokenTypes = {
   DISCORD_ACCESS: 'discord_access',
   DISCORD_REFRESH: 'discord_refresh',
   TWITTER_ACCESS: 'twitter_access',
-  TWITTER_REFRESH: 'twitter_refresh'
+  TWITTER_REFRESH: 'twitter_refresh',
 };
