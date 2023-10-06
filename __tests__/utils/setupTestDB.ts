3e8360ceb24140d7b064581b41ff603698bafab9
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2023-10-06 17:42:39 +0400

    [FORMAT]

--- __tests__/utils/setupTestDB.ts
+++ __tests__/utils/setupTestDB.ts
@@ -1,21 +1,21 @@
-import mongoose from 'mongoose';
-import config from '../../src/config';
+// import mongoose from 'mongoose';
+// import config from '../../src/config';
 
-const setupTestDB = () => {
-    beforeAll(async () => {
-        mongoose.set('strictQuery', false);
-        await mongoose.connect(config.mongoose.serverURL);
-    });
+// const setupTestDB = () => {
+//     beforeAll(async () => {
+//         mongoose.set('strictQuery', false);
+//         await mongoose.connect(config.mongoose.serverURL);
+//     });
 
-    beforeEach(async () => {
-        await Promise.all(
-            Object.values(mongoose.connection.collections).map(async (collection) => collection.deleteMany({})),
-        );
-    });
+//     beforeEach(async () => {
+//         await Promise.all(
+//             Object.values(mongoose.connection.collections).map(async (collection) => collection.deleteMany({})),
+//         );
+//     });
 
-    afterAll(async () => {
-        await mongoose.disconnect();
-    });
-};
+//     afterAll(async () => {
+//         await mongoose.disconnect();
+//     });
+// };
 
-export default setupTestDB;
+// export default setupTestDB;
