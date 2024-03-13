61cc83e3e4fbd632bad8efb3416cbdaad6e43a62
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2024-03-13 12:08:25 +0400

    add Role interface to Community Interface

--- src/interfaces/Community.interface.ts
+++ src/interfaces/Community.interface.ts
@@ -1,11 +1,21 @@
 import { type Model, type Types } from 'mongoose';
 
+export interface ICommunityRoles {
+  roleType: 'view' | 'admin';
+  source: {
+    platform: 'discord';
+    identifierType: 'member' | 'role';
+    identifierValues: string[];
+    platformId: Types.ObjectId;
+  }
+}
 export interface ICommunity {
   name: string;
   avatarURL?: string;
   users: Types.ObjectId[];
   platforms?: Types.ObjectId[];
   tcaAt?: Date;
+  roles?: ICommunityRoles[]
 }
 
 export interface ICommunityUpdateBody {
@@ -14,6 +24,7 @@ export interface ICommunityUpdateBody {
   users?: Types.ObjectId[];
   platforms?: Types.ObjectId[];
   tcaAt?: Date;
+  roles?: ICommunityRoles[]
 }
 
 export interface CommunityModel extends Model<ICommunity> {
