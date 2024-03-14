cdc5650115c6ee07978f8a52f614604363f4d3c3
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2024-03-14 10:50:03 +0400

    add middleware tests

--- __tests__/unit/models/community.mode.test.ts
+++ __tests__/unit/models/community.mode.test.ts
@@ -1,9 +1,9 @@
 import { Community, User, Platform } from '../../../src/models';
 import { ICommunity } from '../../../src/interfaces';
 import { Types } from 'mongoose';
-// import setupTestDB from '../../utils/setupTestDB';
+import setupTestDB from '../../utils/setupTestDB';
 
-// setupTestDB();
+setupTestDB();
 
 describe('Community model', () => {
   describe('Community validation', () => {
@@ -20,31 +20,30 @@ describe('Community model', () => {
     test('should correctly validate a valid community', async () => {
       await expect(new Community(community).validate()).resolves.toBeUndefined();
     });
+    describe('Middlewares', () => {
 
-    // describe('Cascade deletes', () => {
+      test('Pre Remove: should clean up when community is deleted', async () => {
+        const user = new User({ discordId: 'discordId' });
+        await user.save();
 
-    //   test('should clean up when community is deleted', async () => {
-    //     const user = new User({ discordId: 'discordId' });
-    //     await user.save();
+        const community = new Community({ users: [user._id], name: 'community' });
+        await community.save();
+        user.communities?.push(community._id)
 
-    //     const community = new Community({ users: [user._id], name: 'community' });
-    //     await community.save();
-    //     user.communities?.push(community._id)
+        const platform = new Platform({ name: 'platform', community: community._id });
+        await platform.save();
 
-    //     const platform = new Platform({ name: 'platform', community: community._id });
-    //     await platform.save();
+        community.platforms?.push(platform._id);
+        await community.save();
 
-    //     community.platforms?.push(platform._id);
-    //     await community.save();
+        await community.remove();
 
-    //     await community.remove();
+        const userDoc = await User.findById(user._id);
+        expect(userDoc?.communities).not.toContain(community._id);
 
-    //     const userDoc = await User.findById(user._id);
-    //     expect(userDoc?.communities).not.toContain(community._id);
-
-    //     const platformDoc = await Platform.findById(platform._id);
-    //     expect(platformDoc).toBe(null);
-    //   });
-    // });
+        const platformDoc = await Platform.findById(platform._id);
+        expect(platformDoc).toBe(null);
+      });
+    });
   });
 });
--- __tests__/unit/models/platform.model.test.ts
+++ __tests__/unit/models/platform.model.test.ts
@@ -1,9 +1,9 @@
-import { Platform, Community } from '../../../src/models';
+import { Platform, Community ,User} from '../../../src/models';
 import { IPlatform } from '../../../src/interfaces';
 import { Types } from 'mongoose';
-// import setupTestDB from '../../utils/setupTestDB';
+import setupTestDB from '../../utils/setupTestDB';
 
-// setupTestDB();
+setupTestDB();
 
 describe('Platform model', () => {
   describe('Platform validation', () => {
@@ -24,27 +24,50 @@ describe('Platform model', () => {
       await expect(new Platform(platform).validate()).resolves.toBeUndefined();
     });
 
-    // describe('Cascade deletes', () => {
+    describe('Middlewares', () => {
 
-    //   test('should clean up when community is deleted', async () => {
-    //     const community = new Community({ users: [new Types.ObjectId()], name: 'community' });
-    //     await community.save();
+      test('Pre Remove: should clean up when community is deleted', async () => {
+        const community = new Community({ users: [new Types.ObjectId()], name: 'community' });
+        await community.save();
 
-    //     const platform = new Platform({ name: 'platform', community: community._id });
-    //     await platform.save();
+        const platform = new Platform({ name: 'platform', community: community._id });
+        await platform.save();
 
-    //     community.platforms?.push(platform._id);
-    //     await community.save();
+        community.platforms?.push(platform._id);
+        await community.save();
 
-    //     await platform.remove();
+        await platform.remove();
 
-    //     const communityDoc = await Community.findById(community._id);
+        const communityDoc = await Community.findById(community._id);
 
-    //     expect(communityDoc?.platforms).not.toContain(platform._id);
+        expect(communityDoc?.platforms).not.toContain(platform._id);
 
-    //     const platformDoc = await Platform.findById(platform._id);
-    //     expect(platformDoc).toBe(null);
-    //   });
-    // });
+        const platformDoc = await Platform.findById(platform._id);
+        expect(platformDoc).toBe(null);
+      });
+
+      test('Post Save: should add platformId to the community and admin role for the creator of community', async () => {
+        const user = new User({ discordId: 'discordId' });
+        await user.save();
+
+        const community = new Community({ users: [user._id], name: 'community' });
+        await community.save();
+        user.communities?.push(community._id)
+
+        const platform = new Platform({ name: 'platform', community: community._id });
+        await platform.save();
+
+        community.platforms?.push(platform._id);
+        await community.save();
+
+        await community.remove();
+
+        const userDoc = await User.findById(user._id);
+        expect(userDoc?.communities).not.toContain(community._id);
+
+        const platformDoc = await Platform.findById(platform._id);
+        expect(platformDoc).toBe(null);
+      });
+    });
   });
 });
