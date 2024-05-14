d018c87835cf0aadc32d647619efc43b46a7175c
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2024-05-14 15:16:50 +0400

    add enums

---
+++ src/config/enums.ts
@@ -0,0 +1,20 @@
+export enum PlatformNames {
+  Discord = 'discord',
+  Google = 'google',
+  GitHub = 'github',
+  Notion = 'notion',
+  MediaWiki = 'mediaWiki',
+  Twitter = 'twitter',
+}
+
+export enum ModuleNames {
+  Hivemind = 'hivemind',
+}
+
+export enum HivemindPlatformNames {
+  Discord = 'discord',
+  Google = 'google',
+  GitHub = 'github',
+  Notion = 'notion',
+  MediaWiki = 'mediaWiki',
+}
