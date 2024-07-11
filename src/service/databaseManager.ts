822a290dc092b68aa573245b4950a0678defa14a
Behzad Rabiei <behzad.rabiei.77@gmail.com>
2024-07-11 15:39:34 +0400

    refactor: refactor the databaseManger

--- src/service/databaseManager.ts
+++ src/service/databaseManager.ts
@@ -21,6 +21,8 @@ import { type Snowflake } from 'discord.js';
 export default class DatabaseManager {
   private static instance: DatabaseManager;
   private modelCache: Record<string, boolean> = {};
+
+  // Singleton pattern to get the instance of DatabaseManager
   public static getInstance(): DatabaseManager {
     if (typeof DatabaseManager.instance === 'undefined') {
       DatabaseManager.instance = new DatabaseManager();
@@ -28,32 +30,39 @@ export default class DatabaseManager {
     return DatabaseManager.instance;
   }
 
-  public async getGuildDb(tenantId: Snowflake): Promise<Connection> {
-    const dbName = tenantId;
+  // Method to get Guild Database connection
+  public async getGuildDb(guildId: Snowflake): Promise<Connection> {
+    const dbName = guildId;
     const db = mongoose.connection.useDb(dbName, { useCache: true });
     await this.setupModels(db, 'guild');
     return db;
   }
 
-  public async getPlatformDb(tenantId: Snowflake): Promise<Connection> {
-    const dbName = tenantId;
+  // Method to get Platform Database connection
+  public async getPlatformDb(platformId: Snowflake): Promise<Connection> {
+    const dbName = platformId;
     const db = mongoose.connection.useDb(dbName, { useCache: true });
     await this.setupModels(db, 'platform');
     return db;
   }
 
+  // Method to setup models based on database type
   private async setupModels(db: Connection, dbType: 'guild' | 'platform'): Promise<void> {
     if (!this.modelCache[db.name]) {
-      if (dbType === 'platform') {
-        db.model<IHeatMap>('HeatMap', heatMapSchema);
-        db.model<IMemberActivity>('MemberActivity', MemberActivitySchema);
-      } else if (dbType === 'guild') {
-        db.model<IRawInfo>('RawInfo', rawInfoSchema);
-        db.model<IGuildMember>('GuildMember', guildMemberSchema);
-        db.model<IChannel>('Channel', channelSchema);
-        db.model<IRole>('Role', roleSchema);
+      try {
+        if (dbType === 'platform') {
+          db.model<IHeatMap>('HeatMap', heatMapSchema);
+          db.model<IMemberActivity>('MemberActivity', MemberActivitySchema);
+        } else if (dbType === 'guild') {
+          db.model<IRawInfo>('RawInfo', rawInfoSchema);
+          db.model<IGuildMember>('GuildMember', guildMemberSchema);
+          db.model<IChannel>('Channel', channelSchema);
+          db.model<IRole>('Role', roleSchema);
+        }
+        this.modelCache[db.name] = true;
+      } catch (error) {
+        console.error(`Error setting up models for ${db.name}:`, error);
       }
-      this.modelCache[db.name] = true;
     }
   }
 }
