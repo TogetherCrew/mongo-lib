dc608319a118a5d1631a28d3b48e709f602be836
Behzad-rabiei <behzad.rabiei.77@gmail.com>
2025-05-07 09:33:23 +0200

    fix: create seperate db folder

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,5 @@ export * from './models';
 export * from './models/schemas';
 export * from './interfaces';
 export * from './config/enums';
-export * from './databaseManager';
-export * from './connection';
 export * from './repositories';
+export * from './db';
