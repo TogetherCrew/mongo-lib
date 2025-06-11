e61e38c2473fbedf85d9bd1c2c1dcca2e391486b
Behzad-rabiei <behzad.rabiei.77@gmail.com>
2025-06-11 09:04:58 +0200

    fix: fix te issues with commitlint

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,16 +1,17 @@
-import { BaseRepository } from './base.repository';
-import { userRepository } from './user.repository';
-import { tokenRepository } from './token.repository';
-import { makeHeatMapRepository } from './heatMap.repository';
-import { makeRawInfoRepository } from './rawInfo.repository';
-import { makeMemberActivityRepository } from './memberActivity.repository';
-import { makeGuildMemberRepository } from './guildMember.repository';
-import { makeChannelRepository } from './channel.repository';
-import { makeRoleRepository } from './role.repository';
-import { communityRepository } from './community.repository';
-import { platformRepository } from './platform.repository';
 import { announcementRepository } from './announcement.repository';
+import { BaseRepository } from './base.repository';
+import { makeChannelRepository } from './channel.repository';
+import { communityRepository } from './community.repository';
+import { makeGuildMemberRepository } from './guildMember.repository';
+import { makeHeatMapRepository } from './heatMap.repository';
+import { makeMemberActivityRepository } from './memberActivity.repository';
 import { moduleRepository } from './module.repository';
+import { platformRepository } from './platform.repository';
+import { makeRawInfoRepository } from './rawInfo.repository';
+import { makeRoleRepository } from './role.repository';
+import { makeThreadRepository } from './thread.repository';
+import { tokenRepository } from './token.repository';
+import { userRepository } from './user.repository';
 
 export {
   BaseRepository,
@@ -26,4 +27,5 @@ export {
   platformRepository,
   announcementRepository,
   moduleRepository,
+  makeThreadRepository,
 };
