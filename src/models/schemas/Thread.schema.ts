38fe6b4d36f6b9bbf2412867e66cd6e420368fd1
Behzad-rabiei <behzad.rabiei.77@gmail.com>
2025-06-11 10:04:35 +0200

    feat: update thread schema

--- src/models/schemas/Thread.schema.ts
+++ src/models/schemas/Thread.schema.ts
@@ -36,6 +36,76 @@ const threadMetadataSchema = new Schema(
   { _id: false },
 );
 
+const threadMemberSchema = new Schema(
+  {
+    id: {
+      type: String,
+      default: null,
+    },
+    user_id: {
+      type: String,
+      default: null,
+    },
+    join_timestamp: {
+      type: String,
+      required: true,
+    },
+    flags: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    member: {
+      type: Schema.Types.Mixed,
+      default: null,
+    },
+  },
+  { _id: false },
+);
+
+const defaultReactionSchema = new Schema(
+  {
+    emoji_id: {
+      type: String,
+      default: null,
+    },
+    emoji_name: {
+      type: String,
+      default: null,
+    },
+  },
+  { _id: false },
+);
+
+const forumTagSchema = new Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      minlength: 0,
+      maxlength: 20,
+    },
+    moderated: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    emoji_id: {
+      type: String,
+      default: null,
+    },
+    emoji_name: {
+      type: String,
+      default: null,
+    },
+  },
+  { _id: false },
+);
+
 const threadSchema = new Schema<IThread, ThreadModel>(
   {
     id: {
@@ -108,20 +178,48 @@ const threadSchema = new Schema<IThread, ThreadModel>(
       type: Boolean,
       default: false,
     },
-    deletedAt: {
-      type: Date,
+    member: {
+      type: threadMemberSchema,
       default: null,
+    },
+    default_auto_archive_duration: {
+      type: Number,
+      enum: [60, 1440, 4320, 10080],
+      default: null,
+    },
+    permissions: {
+      type: String,
+      default: null,
+    },
+    available_tags: {
+      type: [forumTagSchema],
+      default: [],
+    },
+    default_reaction_emoji: {
+      type: defaultReactionSchema,
+      default: null,
+    },
+    default_thread_rate_limit_per_user: {
+      type: Number,
+      default: null,
+      min: 0,
+      max: 21600,
+    },
+    default_sort_order: {
+      type: Number,
+      enum: [0, 1],
+      default: null,
+    },
+    default_forum_layout: {
+      type: Number,
+      enum: [0, 1, 2],
+      default: 0,
     },
   },
   {
     timestamps: true,
   },
 );
-
-threadSchema.method('softDelete', async function softDelete() {
-  this.deletedAt = new Date();
-  await this.save();
-});
 
 // Plugins
 threadSchema.plugin(toJSON);
